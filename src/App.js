143268db33271f5813e328f7c7183f85e676c624
lgramley <lucy_gramley@brown.edu>
2022-11-14 10:28:43 -0500

    one filter

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 //import logo from './logo.svg';
 import dogData from "./assets/pet-data.json";
 import './App.css';
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Dog from "./components/dog";
 import Buttons from "./components/buttons";
- 
+
 
 dogData.forEach((item) => {
   item.image = process.env.PUBLIC_URL + "/" + item.image;
@@ -13,6 +13,9 @@ dogData.forEach((item) => {
 
 function App() {
   const [dogs, setDogs] = useState([])
+  useEffect(() => {
+    setDogs(dogData)
+  }, [])
   //const [item, setItem] = useState(dogData);
   const rescuePups = [...new Set(dogData.map((Val) => Val.breed))];
 
@@ -20,28 +23,28 @@ function App() {
     setDogs([...dogs, item])
   }
 
+  const filterItem = (breed) => {
+    const newItem = dogData.filter((newVal) => {
+      return newVal.breed === breed;
+      // comparing category for displaying data
+    });
+    console.log(newItem)
+    setDogs(newItem);
+  };
+
   return (
 
     <div className="App">
 
       <h1>Rescue Puppies</h1>
-
-    
-    const filterItem = (curdog) => {
-        const newItem = dogData.filter((newVal) => {
-            return newVal.breed === curdog; 
-        	// comparing category for displaying data
-    });
-    setDogs(newItem);
-  };
-
-      <p>
+      <button onClick={() => filterItem("Labrador")} > Filter by Labrador</button>
+      {/* <p>
         {dogData.map((item, index) => (
           <Buttons item={item} addToCart={addToCart} />
         ))}
-      </p>
+      </p> */}
       <p>
-        {dogData.map((item, index) => (
+        {dogs.map((item, index) => (
           <Dog item={item} addToCart={addToCart} />
         ))}
       </p>
@@ -57,7 +60,7 @@ function App() {
         {/* TODO: render a list of items in the cart */}
       </div>
 
-    </div>
+    </div >
   );
 }
 
--- src/components/buttons.js
+++ src/components/buttons.js
@@ -1,32 +1,33 @@
 
 import React from "react";
-import dogData from "./component/dog";
- 
-const Buttons = ({ setItem, rescuePups}) => {
-  return (
-    <>
-      <div className="d-flex justify-content-center">
-        {rescuePups.map((Val, id) => {
-          return (
-            <button
-              className="btn-dark text-white p-1 px-2 mx-5 btn fw-bold"
-              key={id}
-            >
-              {Val}
-            </button>
-          );
-        })}
-        <button
-          className="btn-dark text-white p-1 px-3 mx-5 fw-bold btn"
-          onClick={() => setItem(dogData)}
-        >
-          All
-        </button>
-       </div>
-    </>
-  );
+import dogData from "./dog";
+
+const Buttons = ({ filterItem, setItem, rescuePups }) => {
+    return (
+        <>
+            <div className="d-flex justify-content-center">
+                {rescuePups.map((Val, id) => {
+                    return (
+                        <button
+                            className="btn-dark text-white p-1 px-2 mx-5 btn fw-bold"
+                            onClick={() => filterItem(Val)}
+                            key={id}
+                        >
+                            {Val}
+                        </button>
+                    );
+                })}
+                <button
+                    className="btn-dark text-white p-1 px-3 mx-5 fw-bold btn"
+                    onClick={() => setItem(dogData)}
+                >
+                    All
+                </button>
+            </div>
+        </>
+    );
 };
- 
+
 export default Buttons;
 
 
